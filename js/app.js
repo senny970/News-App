9c8a02be8d692acc1f99cbf73b80dd0fac922293
senny970 <senny970@gmail.com>
2021-08-04 20:37:16 +0300

    Set up project files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,68 @@
-console.log('hello!');
+// Custom Http Module
+function customHttp() {
+    return {
+        get(url, cb) {
+            try {
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', url);
+                xhr.addEventListener('load', () => {
+                    if (Math.floor(xhr.status / 100) !== 2) {
+                        cb(`Error. Status code: ${xhr.status}`, xhr);
+                        return;
+                    }
+                    const response = JSON.parse(xhr.responseText);
+                    cb(null, response);
+                });
+
+                xhr.addEventListener('error', () => {
+                    cb(`Error. Status code: ${xhr.status}`, xhr);
+                });
+
+                xhr.send();
+            } catch (error) {
+                cb(error);
+            }
+        },
+        post(url, body, headers, cb) {
+            try {
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', url);
+                xhr.addEventListener('load', () => {
+                    if (Math.floor(xhr.status / 100) !== 2) {
+                        cb(`Error. Status code: ${xhr.status}`, xhr);
+                        return;
+                    }
+                    const response = JSON.parse(xhr.responseText);
+                    cb(null, response);
+                });
+
+                xhr.addEventListener('error', () => {
+                    cb(`Error. Status code: ${xhr.status}`, xhr);
+                });
+
+                if (headers) {
+                    Object.entries(headers).forEach(([key, value]) => {
+                        xhr.setRequestHeader(key, value);
+                    });
+                }
+
+                xhr.send(JSON.stringify(body));
+            } catch (error) {
+                cb(error);
+            }
+        },
+    };
+}
+
+// Init http module
+const http = customHttp();
+
+const newsService = (function () {
+    const apiKey = 'f8a92a36ea064c52ae43f8f6f654ec1d';
+    const apiUrl = 'https://news-api-v2.herokuapp.com';
+})
+
+//  init selects
+document.addEventListener('DOMContentLoaded', function () {
+    M.AutoInit();
+});
