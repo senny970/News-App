3e4bdda6b80835c782da39e9e7a6ec86dd8ee1fd
Alexander <senny970@gmail.com>
2021-08-06 10:30:57 +0300

    add search news by text, add preloader, add clear body after form parameters changes

--- js/app.js
+++ js/app.js
@@ -57,14 +57,26 @@ function customHttp() {
 // Init http module
 const http = customHttp();
 
+//Elements
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const searchInput = form.elements['search'];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    loadNews();
+})
+
 const newsService = (function () {
+    //https://newsapi.org/sources
     const apiKey = 'f8a92a36ea064c52ae43f8f6f654ec1d';
-    const apiUrl = 'https://news-api-v2.herokuapp.com';
+    // const apiUrl = 'https://news-api-v2.herokuapp.com';
+    const apiUrl = 'https://newsapi.org/v2';
 
     return {
         topHeadlines(country = 'ua', cb) {
             http.get(`${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`, cb);
-        },
+                },
         everything(query, cb) {
             http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
         }
@@ -79,17 +91,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //Load news
 function loadNews() {
-    newsService.topHeadlines('us', onGetResponse);
-}
+    showPreloader();
 
+    const country = countrySelect.value;
+    const searchText = searchInput.value;
+
+    if (!searchText) {
+        newsService.topHeadlines(country, onGetResponse);
+    } else {
+        newsService.everything(searchText, onGetResponse);
+    }
+}
 //On get response from server
 function onGetResponse(err, res) {
+    removePreloader();
+
+    if (err) {
+        showAlert(err, 'error-msg');
+        return;
+    }
+
+    if (!res.articles.length) {
+        showAlert('News not found!', 'error-msg');
+        return;
+    }
+
     renderNews(res.articles)
 }
 
 //Render news
 function renderNews(news) {
     const newsContainer = document.querySelector('.news-container .row');
+
+    if (newsContainer.children.length) {
+        clearContainer(newsContainer);
+    }
+
     let fragment = '';
     news.forEach(newsItem => {
         const element = newsTemplate(newsItem);
@@ -117,4 +154,35 @@ function newsTemplate({urlToImage, title, url, description}) {
             </div>
         </div>
     `
+}
+
+//Message
+function showAlert(msg, type = 'success') {
+    M.toast({html: msg, classes: type});
+}
+
+//Clear container
+function clearContainer(container) {
+    //container.innerHTML = '';
+    let child = container.lastElementChild;
+
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
+
+//Preloader
+function showPreloader() {
+    document.body.insertAdjacentHTML('afterbegin', '  ' +
+        '<div class="progress">\n' +
+        '   <div class="indeterminate"></div>\n' +
+        '</div>'
+    );
+}
+
+//Remove Preloader
+function removePreloader() {
+    const loader = document.querySelector('.progress');
+    if (loader) loader.remove();
 }
