19332230335879188f9c4e990703911474e8bae8
Alexander <senny970@gmail.com>
2021-08-09 10:22:57 +0300

    add alerts

--- js/app.js
+++ js/app.js
@@ -88,6 +88,7 @@ const newsService = (function () {
 //  init selects
 document.addEventListener('DOMContentLoaded', function () {
     loadNews();
+
 });
 
 //Load news
@@ -110,12 +111,12 @@ function onGetResponse(err, res) {
 
     if (err) {
         console.error(err);
-        showAlert(err, 'error-msg');
+        showAlert(err, 'alert-danger');
         return;
     }
 
     if (!res.articles.length) {
-        showAlert('News not found!', 'error-msg');
+        showAlert('News not found!', 'alert-warning');
         return;
     }
 
@@ -156,8 +157,17 @@ function newsTemplate({urlToImage, title, url, description}) {
 }
 
 //Message
-function showAlert(msg, type = 'success') {
-    M.toast({html: msg, classes: type});
+function showAlert(msg, type = 'alert-danger', title = '') {
+    document.body.innerHTML += `
+        <div class="alert ${type} alert-dismissible fade show" role="alert" style="position: fixed; left: 84.8%; top: 70px">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">\n' +
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>\n' +
+            </svg>
+            <strong>${title}</strong> ${msg}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        `;
+return 0;
 }
 
 //Clear container
